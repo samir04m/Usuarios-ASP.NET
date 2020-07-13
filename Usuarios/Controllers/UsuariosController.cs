c32a2d18af6fcac67b9c667006568a3da4593190
Samir Mejia <samir04m@gmail.com>
2020-07-13 18:30:56 -0500

    Creación de la vista y controlador de Usuario

---
+++ Usuarios/Controllers/UsuariosController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Usuarios.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public IActionResult Index(int id, String name, int age)
+        {
+            // https://localhost:44371/Usuarios/Index?name=Samir&age=22
+
+            ViewData["id"] = id;
+            String datos = name + "  " + age;
+            return View("Index", datos);
+        }
+    }
+}
