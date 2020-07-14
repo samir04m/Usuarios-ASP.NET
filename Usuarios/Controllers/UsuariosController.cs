edd4afbee47ffb57bda867d4cf5931f02a1b9681
Samir Mejia <samir04m@gmail.com>
2020-07-14 15:34:26 -0500

    Modificación de rutas

--- Usuarios/Controllers/UsuariosController.cs
+++ Usuarios/Controllers/UsuariosController.cs
@@ -6,8 +6,12 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Usuarios.Controllers
 {
+    //[Route("/users")]
     public class UsuariosController : Controller
     {
+        //[HttpGet]
+        [Route("/users/home")]
+        [Route("/users/profile/{name}")]
         public IActionResult Index(int id, String name, int age)
         {
             // https://localhost:44371/Usuarios/Index?name=Samir&age=22
@@ -15,6 +19,11 @@ namespace Usuarios.Controllers
             ViewData["id"] = id;
             String datos = name + "  " + age;
             return View("Index", datos);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
         }
     }
 }
