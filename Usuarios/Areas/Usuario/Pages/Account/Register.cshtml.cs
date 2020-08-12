212efeb60d39e9b7fdee77ccc2095fedc3e5357b
Samir Mejia <samir04m@gmail.com>
2020-08-12 18:44:31 -0500

    Creación de la interfaz de Registro de Usuario

--- Usuarios/Areas/Usuario/Pages/Account/Register.cshtml.cs
+++ Usuarios/Areas/Usuario/Pages/Account/Register.cshtml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -9,11 +10,44 @@ namespace Usuarios.Areas.Usuario.Pages.Account
 {
     public class RegisterModel : PageModel
     {
-        public string Message { get; set; }
-
         public void OnGet(string data)
         {
-            Message = data;
+        }
+        
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public string Email { get; set; }
+
+            [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            [StringLength(100, ErrorMessage = "El número de caracteres de {0} debe ser al menos {2}.", MinimumLength = 4)]
+            public string Password { get; set;  }
+
+            [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+            [DataType(DataType.Password)]
+            [Compare("Password", ErrorMessage = "Las contraseñas no son iguales.")]
+            public string ConfirmPassword { get; set; }
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            else
+            {
+                ModelState.AddModelError("Input.Email", "Se ha generado un error en el servidor");
+            }
+            var data = Input;
+            return Page();
         }
     }
 }
