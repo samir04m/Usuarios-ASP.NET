e270b896db7f3fb4bec049440422cce9b1936f8e
Samir Mejia <samir04m@gmail.com>
2020-08-13 15:22:09 -0500

    Procedimiento para guardar ususarios en la base de datos

--- Usuarios/Areas/Usuario/Pages/Account/Register.cshtml.cs
+++ Usuarios/Areas/Usuario/Pages/Account/Register.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -10,8 +11,19 @@ namespace Usuarios.Areas.Usuario.Pages.Account
 {
     public class RegisterModel : PageModel
     {
+        private UserManager<IdentityUser> _userManager;
+        private static InputModel _input = null;
+        public RegisterModel(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public void OnGet(string data)
         {
+            if (_input != null)
+            {
+                Input = _input;
+            }
         }
         
         [BindProperty]
@@ -34,15 +46,69 @@ namespace Usuarios.Areas.Usuario.Pages.Account
             [DataType(DataType.Password)]
             [Compare("Password", ErrorMessage = "Las contraseñas no son iguales.")]
             public string ConfirmPassword { get; set; }
+
+            public string ErrorMessage { get; set; }
         }
 
-        public IActionResult OnPost()
+        public async Task<IActionResult> OnPostAsync()
         {
+            if (await RegisterUserAsync())
+            {
+                return Redirect("/Principal/Principal?area=Principal");
+            }
+            else
+            {
+                return Redirect("/Usuario/Register");
+            }
+        }
+
+        private async Task<bool> RegisterUserAsync()
+        {
+            var run = false;
             if (ModelState.IsValid)
             {
-
+                var userList = _userManager.Users.Where(u => u.Email.Equals(Input.Email)).ToList();
+                if (userList.Count.Equals(0))
+                {
+                    var user = new IdentityUser
+                    {
+                        UserName = Input.Email,
+                        Email = Input.Email
+                    };
+                    var result = await _userManager.CreateAsync(user, Input.Password);
+                    if (result.Succeeded)
+                    {
+                        run = true;
+                    }
+                    else
+                    {
+                        foreach (var item in result.Errors)
+                        {
+                            Input = new InputModel
+                            {
+                                ErrorMessage = item.Description,
+                                Email = Input.Email
+                            };
+                        }
+                        _input = Input;
+                        run = false;
+                    }
+                }
+                else
+                {
+                    Input = new InputModel
+                    {
+                        ErrorMessage = $"El {Input.Email} ya esta registrado",
+                        Email = Input.Email
+                    };
+                    _input = Input;
+                    run = false;
+                }
             }
-            return Page();
+            return run;
         }
+
+
+
     }
 }
