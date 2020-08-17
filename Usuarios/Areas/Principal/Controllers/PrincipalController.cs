1d453a5b88b1f689048165bd08d6cdc4d1f3caa6
Samir Mejia <samir04m@gmail.com>
2020-08-17 13:40:23 -0500

    Obteniendo la información del usuario logeado con UserManager

--- Usuarios/Areas/Principal/Controllers/PrincipalController.cs
+++ Usuarios/Areas/Principal/Controllers/PrincipalController.cs
@@ -18,6 +18,7 @@ namespace Usuarios.Areas.Principal.Controllers
     public class PrincipalController : Controller
     {
         private SignInManager<IdentityUser> _signInManager;
+        private UserManager<IdentityUser> _userManager;
 
         public PrincipalController(
             UserManager<IdentityUser> userManager,
@@ -25,18 +26,23 @@ namespace Usuarios.Areas.Principal.Controllers
             RoleManager<IdentityRole> roleManager)
         {
             _signInManager = signInManager;
+            _userManager = userManager;
         }
 
         //[Authorize(Roles = "Admin, User")]
         //[Authorize(Policy = "Authorization")]
-        public IActionResult Principal()
+        public async Task<IActionResult> Principal()
         {
             if (_signInManager.IsSignedIn(User))
             {
                 //var user = HttpContext.Session.GetString("User");
                 //var age = HttpContext.Session.GetInt32("Age");
                 //var dataUser = JsonConvert.DeserializeObject<TUsers>(user);
-                var user = User.Claims.FirstOrDefault(u => u.Type.Equals(ClaimTypes.Name)).Value;
+                //var user = User.Claims.FirstOrDefault(u => u.Type.Equals(ClaimTypes.Name)).Value;
+
+                var id = _userManager.GetUserId(User);
+                var user = await _userManager.FindByIdAsync(id);
+                var roles = await _userManager.GetRolesAsync(user);
                 return View();
             }
             else
